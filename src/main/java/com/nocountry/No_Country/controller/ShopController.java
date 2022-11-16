eb884cddb92d8fa8b0607e3c4bf02ee1e022e5ba
LeandroVizgarra <leandro.elias.vizgarra@gmail.com>
2022-11-16 19:21:07 -0300

    TODO actualizados

--- src/main/java/com/nocountry/No_Country/controller/ShopController.java
+++ src/main/java/com/nocountry/No_Country/controller/ShopController.java
@@ -1,26 +1,20 @@
 package com.nocountry.No_Country.controller;
 
-import com.nocountry.No_Country.dtos.UserDTO;
-import com.nocountry.No_Country.service.LocationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/shop")
 public class ShopController {
 
 
+    //TODO Crear las funciones faltantes de Mappers.
 
-    //TODO Comprobar y configurar las relaciones entre clases.
-    //TODO Configurar en las properties las credenciales de la BBDD a usar.
-    //TODO Verificar la correcta creacion en la BBDD.
-    //TODO Configurar gitIgnore en caso de ser necesario.
-
-    //TODO Crear las funciones en los Mappers DTO a entidad y entidad a DTO.
     //TODO Realizar primeros test para comprobar que la relaciones funcionan entre si.
+
     //TODO CORS protocol.
+
+    //TODO Crear DTOS en base a necesidades para evitar información no util.
 
 
 }
