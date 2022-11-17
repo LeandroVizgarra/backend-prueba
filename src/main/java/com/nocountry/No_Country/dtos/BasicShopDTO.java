435a993e92862eb7a29d2e9cef9b94da360703e2
AleVedoya <alejandravedoya@hotmail.com>
2022-11-17 09:56:54 -0300

    Create basics Dto

---
+++ src/main/java/com/nocountry/No_Country/dtos/BasicShopDTO.java
@@ -0,0 +1,14 @@
+package com.nocountry.No_Country.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BasicShopDTO {
+    private String name;
+    private Long phoneNumber;
+    private Long locationId;
+}
